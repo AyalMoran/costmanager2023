866ea79f59af82aa5c1d5d6d366a962c705147a3
AyalMoran <77284947+AyalMoran@users.noreply.github.com>
2023-06-22 18:13:37 +0300

    Update data-base.js

--- models/data-base.js
+++ models/data-base.js
@@ -1,3 +1,16 @@
+//  Yael Zvadviker - id:208418384
+//  Ofek Avigdor  - id:307837807
+//  Ayal Moran - id:311148522 
+//
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
 console.log('start file database');
 const mongoose = require('mongoose');
 
